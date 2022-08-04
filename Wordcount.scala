3b97df5d474f01a1f5a7feaa60bfeb3df543f310
Vineeth061 <102545056+Vineeth061@users.noreply.github.com>
2022-08-04 21:20:26 +0530

    Create Wordcount.scala
    sbt compile
    sbt "run https://path/wordcount.txt"

---
+++ Wordcount.scala
@@ -0,0 +1,47 @@
+1.
+import scala.io.Source
+
+object Wc {
+  def main(args:Array[String]){
+    if(args.length!=1){
+      System.err.println("Usage:sbt run \"String\"")
+      System.exit(1)
+    }
+    
+    val fn = args(0)
+    val wc = scala.collection.mutable.Map[String,Int]()
+    
+    for(line<-Source.fromFile(fn).getLine)
+      for(words<-line.split(" "))
+        if(wc.contains(word))
+          wc(word)=wc(word)+1
+        else
+          wc(word)=1
+    
+    for((k,v)<-wc)
+      println("Word %s occurs %d times\n",k,v)
+    println(wc)
+  }
+}
+
+
+
+2.
+import scala.io.Source
+object scalaWordCount{
+	def main(args: Array[String]){
+		if(args.length != 1){
+			System.err.println("Usage: sbt run \"word.txt\"")
+			System.exit(1)
+		}
+		val filename=args(0)
+		val wordcount = scala.collection.mutable.Map[String,Int]()
+		for(line<-Source.fromFile("words.txt").getLines)
+			for(word <- line.split(" "))
+				wordcount(word) = if(wordcount.contains(word)) wordcount(word) + 1 else 1
+			
+		for((k,v) <- wordcount)
+			printf("Word %s occurs %d times\n", k,v)
+		println(wordcount)
+	}
+}
